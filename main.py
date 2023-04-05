cb7f4c5c12a0aed6218456f9a84049ff9f8d6aad
fellipe1908 <128155054+fellipe1908@users.noreply.github.com>
2023-04-05 18:22:36 +0000

    Exercicio Resolvido

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ a =int( input("DIgite o primeiro angulo: "))
 b =int( input("DIgite o segundo angulo: "))
 c =int( input("DIgite o terceiro angulo: "))
 r = int(a + b +c)
-if a < 1 and b < 1 and c < 1:
+if a < 1 or b < 1 or c < 1:
+    print("Não é um triângulo")
+else:
     if r == 180 :
         if  a == b and a == c and c == b:
             print("Equilátero")
         elif a == b or b == c or a == c:
             print("Isósceles")
-        else:
+        else: 
             print("Escaleno")
     else:
-        print("Não é um triângulo")
-else:
-    print("Não é um triângulo")
+        print("Não é um triângulo")    
+    
+    
+    
+    
+    
+    
+    
+
+    
