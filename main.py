433b822c56a848f7cb52922af049cff7df315fc0
fellipe1908 <128155054+fellipe1908@users.noreply.github.com>
2023-04-14 10:58:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ a =int( input("DIgite o primeiro angulo: "))
 b =int( input("DIgite o segundo angulo: "))
 c =int( input("DIgite o terceiro angulo: "))
 r = int(a + b +c)
-if a + b > c or a + c > b or b + c > a:
+if a + b > c and a + c > b and b + c > a:
     if  a == b and a == c and c == b:
             print("Equilátero")
     elif a == b or b == c or a == c:
